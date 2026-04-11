e914cbef27a93338873b33a619e07f22d0b36e9a
Fiaz26 <fiaznns26@gmail.com>
2026-04-11 17:24:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 from fastapi import FastAPI
 
 app = FastAPI()
