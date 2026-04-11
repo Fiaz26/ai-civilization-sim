24d00b14bd37f8d44afa5a7c93077abff12fda05
Fiaz26 <fiaznns26@gmail.com>
2026-04-11 20:41:24 +0500

    Add user creation and verification functions

--- auth.py
+++ auth.py
@@ -1,1 +1,20 @@
+from database import cursor, conn
 
+def create_user(email, password):
+    try:
+        cursor.execute(
+            "INSERT INTO users (email, password) VALUES (?, ?)",
+            (email, password)
+        )
+        conn.commit()
+        return True
+    except:
+        return False
+
+
+def verify_user(email, password):
+    cursor.execute(
+        "SELECT * FROM users WHERE email=? AND password=?",
+        (email, password)
+    )
+    return cursor.fetchone()
