70f78d65dd17712c160b0b989685096f56a54b64
Fiaz26 <fiaznns26@gmail.com>
2026-04-11 17:00:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,23 @@ def step():
         "companies": 2 + tick // 2,
         "alive": True
     }
+from pydantic import BaseModel
+
+class User(BaseModel):
+    email: str
+    password: str
+
+fake_users = {}
+@app.post("/signup")
+def signup(user: User):
+    if user.email in fake_users:
+        return {"error": "User exists"}
+
+    fake_users[user.email] = user.password
+    return {"message": "signup success"}
+    @app.post("/login")
+def login(user: User):
+    if fake_users.get(user.email) == user.password:
+        return {"message": "login success", "api_key": user.email}
+    return {"error": "invalid credentials"}
+    
