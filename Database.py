9407bba4a459ffee6b5d2f4a2485f60bfc140c95
Fiaz26 <fiaznns26@gmail.com>
2026-04-11 20:38:41 +0500

    Change database file name and update users table
    Updated database connection string and modified users table structure.

--- Database.py
+++ Database.py
@@ -1,37 +1,15 @@
-# database.py
 import sqlite3
 
-conn = sqlite3.connect("saas.db", check_same_thread=False)
+conn = sqlite3.connect("db.sqlite", check_same_thread=False)
 cursor = conn.cursor()
 
 def init_db():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT,
+        email TEXT PRIMARY KEY,
         password TEXT,
         plan TEXT DEFAULT 'free',
-        api_key TEXT
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS usage (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        api_key TEXT,
-        endpoint TEXT,
-        count INTEGER DEFAULT 0,
-        date TEXT
+        credits INTEGER DEFAULT 10
     )
     """)
     conn.commit()
-  cursor.execute("""
-CREATE TABLE IF NOT EXISTS payments (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    api_key TEXT,
-    method TEXT,
-    amount REAL,
-    txn_id TEXT,
-    status TEXT
-)
-""")
