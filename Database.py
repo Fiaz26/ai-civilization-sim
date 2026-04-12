1dad18f71f9caecaf8956f7e750deb1cf1b31f6f
Fiaz26 <fiaznns26@gmail.com>
2026-04-12 13:26:17 +0500

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,1 +1,20 @@
+import sqlite3
 
+def get_db():
+    conn = sqlite3.connect("db.sqlite", check_same_thread=False)
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def init_db():
+    conn, cursor = get_db()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        email TEXT PRIMARY KEY,
+        password TEXT,
+        credits INTEGER DEFAULT 10
+    )
+    """)
+    conn.commit()
+    conn.close()
